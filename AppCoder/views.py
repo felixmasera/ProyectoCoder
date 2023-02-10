9d487a58c5a036aec538d4e12e03bfa9776b6acf
“FelixMasera” <felixmasera@gmail.com>
2023-02-10 19:42:10 -0300

    Arreglo de rutas

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 #from AppCoder.models import Curso
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse('vista inicio')
+    return render(request, 'AppCoder/inicio.html')
 
 
 def cursos(request):
-    return HttpResponse('vista cursos')
+    return render(request,'AppCoder/cursos.html')
 
 
 def profesores(request):
-    return HttpResponse('vista profesores')
+    return render(request,'AppCoder/profesores.html')
 
 
 def estudiantes(request):
-    return HttpResponse('vista estudiantes')
+    return render(request,'AppCoder/estudiantes.html')
 
 
 def entregables(request):
-    return HttpResponse('vista entregables')
+    return render(request,'AppCoder/entregables.html')
